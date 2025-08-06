43de4eb0bc930cf0f5f4f74098fdb389aa9d48e7
Bruno Panizzi <bruno@ibotiadvogados.com.br>
2025-08-06 15:51:38 -0300

    add table component to ui package

--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -2,6 +2,7 @@ export { Button, ButtonProps } from "./components/button"
 export { Input, InputProps } from "./components/input"
 import * as BaseDialog from "./components/dialog"
 import * as BaseSelect from "./components/select"
+import * as BaseTable from "./components/table"
 
 export const Dialog = {
   Root: BaseDialog.Dialog,
@@ -25,4 +26,15 @@ export const Select = {
   Label: BaseSelect.SelectLabel,
   Item: BaseSelect.SelectItem,
   Separator: BaseSelect.SelectSeparator,
+}
+
+export const Table = {
+  Root: BaseTable.Table,
+  Body: BaseTable.TableBody,
+  Caption: BaseTable.TableCaption,
+  Cell: BaseTable.TableCell,
+  Head: BaseTable.TableHead,
+  Header: BaseTable.TableHeader,
+  Row: BaseTable.TableRow,
+  Footer: BaseTable.TableFooter,
 }
