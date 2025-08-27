cbc7b6b4d167dd23bc85eb69a6d052233f7eb175
Bruno Panizzi <bruno@ibotiadvogados.com.br>
2025-08-27 15:17:59 -0300

    use bcrypt hash for auauth

--- apps/auauth/src/hashing.ts
+++ apps/auauth/src/hashing.ts
@@ -1,9 +1,9 @@
 import { password } from "bun"
 
 export function hashPassword(pswd: string) {
-  return password.hashSync(pswd)
+  return password.hashSync(pswd, { algorithm: 'bcrypt' })
 }
 
 export function verifyPassword(pswd: string, hash: string) {
-  return password.verifySync(pswd, hash)
+  return password.verifySync(pswd, hash, "bcrypt")
 }
